b75e0274bd958bb7994c93f4d25e87e852f3eb00
Kathy Brade <brade@pearlcrescent.com>
2016-06-28 15:13:05 -0400

    Bug 19273: Avoid JavaScript patching of the external app helper dialog.
    When handling an external URI or downloading a file, invoke Torbutton's
    external app blocker component (which will present a download warning
    dialog unless the user has checked the "Automatically download files
    from now on" box).
    
    For e10s compatibility, avoid using a modal dialog and instead use
    a callback interface (nsIHelperAppWarningLauncher) to allow Torbutton
    to indicate the user's desire to cancel or continue each request.
    
    Other bugs fixed:
     Bug 21766: Crash with e10s enabled while trying to download a file
     Bug 21886: Download is stalled in non-e10s mode
     Bug 22471: Downloading files via the PDF viewer download button is broken
     Bug 22472: Fix FTP downloads when external helper app dialog is shown
     Bug 22610: Avoid crashes when canceling external helper app downloads
     Bug 22618: Downloading pdf file via file:/// is stalling

--- uriloader/exthandler/nsExternalHelperAppService.h
+++ uriloader/exthandler/nsExternalHelperAppService.h
@@ -218,6 +218,7 @@ class nsExternalHelperAppService : public nsIExternalHelperAppService,
  */
 class nsExternalAppHandler final : public nsIStreamListener,
                                    public nsIHelperAppLauncher,
+                                   public nsIHelperAppWarningLauncher,
                                    public nsIBackgroundFileSaverObserver,
                                    public nsINamed {
  public:
@@ -225,6 +226,7 @@ class nsExternalAppHandler final : public nsIStreamListener,
   NS_DECL_NSISTREAMLISTENER
   NS_DECL_NSIREQUESTOBSERVER
   NS_DECL_NSIHELPERAPPLAUNCHER
+  NS_DECL_NSIHELPERAPPWARNINGLAUNCHER
   NS_DECL_NSICANCELABLE
   NS_DECL_NSIBACKGROUNDFILESAVEROBSERVER
   NS_DECL_NSINAMED
@@ -496,6 +498,7 @@ class nsExternalAppHandler final : public nsIStreamListener,
   nsCOMPtr<nsITransfer> mTransfer;
 
   nsCOMPtr<nsIHelperAppLauncherDialog> mDialog;
+  nsCOMPtr<nsIHelperAppWarningDialog> mWarningDialog;
 
   /**
 
