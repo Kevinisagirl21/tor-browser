cf1ae8d74586089f1084edfea71d4bb1c960d05f
Kathy Brade <brade@pearlcrescent.com>
2018-08-08 11:34:40 -0400

    Bug 26961: New user onboarding.
    Reuse the Firefox onboarding mechanism with minimal changes.
    Localizable strings are pulled in from Torbutton (if Torbutton is
      not installed, we lack about:tor and no tour will be shown).
    Replace SVG images with PNGs (see bug 27002),
    For defense in depth, omit include OnboardingTelemetry.jsm entirely.
    Added support for the following UITour page event:
      torBrowserOpenSecuritySettings
    
    Also fix bug 27403: the onboarding bubble is not always displayed.
    
    Arthur suggested to make the onboarding bubble visible on displays with
    less than 960px width available, so we choose 200px instead.
    
    Also fix bug 28628: Change onboarding Security panel to open new
    Security Level panel.
    
    Also fix bug 27484: Improve navigation within onboarding.
    
    Bug 27082: enable a limited UITour
    
    Disallow access to UITour functionality from all pages other than
      about:home, about:newtab, and about:tor.
    Implement a whitelist mechanism for page actions.
    
    Bug 26962 - implement new features onboarding (part 1).
    
    Add an "Explore" button to the "Circuit Display" panel within new user
    onboarding which opens the DuckDuckGo .onion and then guides users through
    a short circuit display tutorial.
    
    Allow a few additional UITour actions while limiting as much as possible
    how it can be used.
    
    Tweak the UITour styles to match the Tor Browser branding.
    
    All user interface strings are retrieved from Torbutton's
    browserOnboarding.properties file.
    
    Bug 27486 Avoid about:blank tabs when opening onboarding pages.
    
    Instead of using a simple <a href>, programmatically open onboarding
    web pages by using tabBrowser.addTab(). The same technique is now
    used for "See My Path", "See FAQs", and "Visit an Onion".
    
    Bug 29768: Introduce new features to users
    
    Add an "update" tour for the Tor Browser 8.5 release that contains two
    panels: Toolbar and Security (with appropriate description text and
    images).
    
    Display an attention-grabbing dot on the onboarding text bubble when
    the update tour is active. The animation lasts for 14 seconds.
    
    Bug 31768: Introduce toolbar and network settings changes in onboarding
    
    Update the "Tor Network" onboarding page to include a note that
    settings can now be accessed via the application preferences and
    add an "Adjust Your Tor Network Settings" action button which opens
    about:preferences#tor.
    
    Replace the Tor Browser 8.5 "update" onboarding tour with a 9.0 one
    that includes the revised "Tor Network" page and a revised "Toolbar"
    page. The latter explains that Torbutton's toolbar item has been
    removed ("Goodbye Onion Button") and explains how to access the
    New Identity feature using the hamburger menu and new toolbar item.
    
    Bug 34321 - Add Learn More onboarding item

--- intl/strres/nsStringBundle.cpp
+++ intl/strres/nsStringBundle.cpp
@@ -77,6 +77,7 @@ static const char kContentBundles[][52] = {
     "chrome://global/locale/svg/svg.properties",
     "chrome://global/locale/xul.properties",
     "chrome://necko/locale/necko.properties",
+    "chrome://torbutton/locale/onboarding.properties",
 };
 
 static bool IsContentBundle(const nsCString& aUrl) {
