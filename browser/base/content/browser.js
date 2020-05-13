e2c9144979c2619df54e55b4cf35d543ed52e9a4
Alex Catarineu <acat@torproject.org>
2020-05-13 11:13:24 +0200

    Bug 34196: Update site info URL with the onion name

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -5473,6 +5473,7 @@ var XULBrowserWindow = {
     // Don't need to do anything if the data we use to update the UI hasn't
     // changed
     let uri = gBrowser.currentURI;
+    let onionAliasURI = gBrowser.selectedBrowser.currentOnionAliasURI;
     let spec = uri.spec;
     if (this._state == aState && this._lastLocation == spec) {
       // Switching to a tab of the same URL doesn't change most security
@@ -5490,7 +5491,7 @@ var XULBrowserWindow = {
     try {
       uri = Services.io.createExposableURI(uri);
     } catch (e) {}
-    gIdentityHandler.updateIdentity(this._state, uri);
+    gIdentityHandler.updateIdentity(this._state, uri, onionAliasURI);
   },
 
   // simulate all change notifications after switching tabs
