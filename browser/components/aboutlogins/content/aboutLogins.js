a9b77f1df7d758e17704e34d9fed6086df5176d8
Kathy Brade <brade@pearlcrescent.com>
2020-07-14 11:15:07 -0400

    Bug 33852: Clean up about:logins (LockWise) to avoid mentioning sync, etc.
    Hide elements on about:logins that mention sync, "Firefox LockWise", and
    Mozilla's LockWise mobile apps.
    
    Disable the "Create New Login" button when security.nocertdb is true.

--- browser/components/aboutlogins/content/aboutLogins.js
+++ browser/components/aboutlogins/content/aboutLogins.js
@@ -19,6 +19,9 @@ const gElements = {
   get loginFooter() {
     return this.loginItem.shadowRoot.querySelector("login-footer");
   },
+  get createNewLoginButton() {
+    return this.loginList.shadowRoot.querySelector(".create-login-button");
+  },
 };
 
 let numberOfLogins = 0;
@@ -100,6 +103,9 @@ window.addEventListener("AboutLoginsChromeToContent", event => {
       gElements.loginList.setSortDirection(event.detail.value.selectedSort);
       document.documentElement.classList.add("initialized");
       gElements.loginList.classList.add("initialized");
+      if (!event.detail.value.canCreateLogins) {
+        gElements.createNewLoginButton.disabled = true;
+      }
       break;
     }
     case "ShowLoginItemError": {
