639a146e852da3cdc620eb73b51af3188098b7b4
Matthew Finkel <sysrqb@torproject.org>
2020-09-14 02:52:28 +0000

    Bug 40125: Expose Security Level pref in GeckoView

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoRuntimeSettings.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoRuntimeSettings.java
@@ -446,6 +446,17 @@ public final class GeckoRuntimeSettings extends RuntimeSettings {
             getSettings().mForceUserScalable.set(flag);
             return this;
         }
+
+        /**
+         * Set security level.
+         *
+         * @param level A value determining the security level. Default is 0.
+         * @return This Builder instance.
+         */
+        public @NonNull Builder torSecurityLevel(final int level) {
+            getSettings().mTorSecurityLevel.set(level);
+            return this;
+        }
     }
 
     private GeckoRuntime mRuntime;
@@ -494,6 +505,8 @@ public final class GeckoRuntimeSettings extends RuntimeSettings {
             "browser.ui.zoom.force-user-scalable", false);
     /* package */ final Pref<Boolean> mAutofillLogins = new Pref<Boolean>(
         "signon.autofillForms", true);
+    /* package */ final Pref<Integer> mTorSecurityLevel = new Pref<>(
+        "extensions.torbutton.security_slider", 4);
 
     /* package */ int mPreferredColorScheme = COLOR_SCHEME_SYSTEM;
 
@@ -1129,6 +1142,26 @@ public final class GeckoRuntimeSettings extends RuntimeSettings {
     public @NonNull GeckoRuntimeSettings setLoginAutofillEnabled(
             final boolean enabled) {
         mAutofillLogins.commit(enabled);
+        return this;
+    }
+
+    /**
+     * Gets the current security level.
+     *
+     * @return current security protection level
+     */
+    public int getTorSecurityLevel() {
+        return mTorSecurityLevel.get();
+    }
+
+    /**
+     * Sets the Tor Security Level.
+     *
+     * @param level security protection level
+     * @return This GeckoRuntimeSettings instance.
+     */
+    public @NonNull GeckoRuntimeSettings setTorSecurityLevel(final int level) {
+        mTorSecurityLevel.commit(level);
         return this;
     }
 
