0776eab4502ec4fb8d7eeb36f7a802ac20a13ca7
Richard Pospesel <richard@torproject.org>
2021-04-28 23:09:34 -0500

    Bug 27476: Implement about:torconnect captive portal within Tor Browser
    - implements new about:torconnect page as tor-launcher replacement
    - adds new torconnect component to browser
    - tor process management functionality remains implemented in tor-launcher through the TorProtocolService module
    - adds warning/error box to about:preferences#tor when not connected to tor
    
    Bug 40773: Update the about:torconnect frontend page to match additional UI flows.
    
    Bug 41608: Add a toolbar status button and a urlbar "Connect" button.

---
+++ browser/components/torconnect/TorConnectChild.sys.mjs
@@ -0,0 +1,5 @@
+// Copyright (c) 2021, The Tor Project, Inc.
+
+import { RemotePageChild } from "resource://gre/actors/RemotePageChild.sys.mjs";
+
+export class TorConnectChild extends RemotePageChild {}
