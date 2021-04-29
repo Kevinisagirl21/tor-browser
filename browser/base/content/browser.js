4f9bfd44fd7110afb9c42413f18e1c1a7ae1b9cc
Richard Pospesel <richard@torproject.org>
2021-04-28 23:09:34 -0500

    Bug 27476: Implement about:torconnect captive portal within Tor Browser
    - implements new about:torconnect page as tor-launcher replacement
    - adds new torconnect component to browser
    - tor process management functionality remains implemented in tor-launcher through the TorProtocolService module
    - adds warning/error box to about:preferences#tor when not connected to tor
    
    Bug 40773: Update the about:torconnect frontend page to match additional UI flows.
    
    Bug 41608: Add a toolbar status button and a urlbar "Connect" button.

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -78,6 +78,9 @@ XPCOMUtils.defineLazyModuleGetters(this, {
   TabModalPrompt: "chrome://global/content/tabprompts.jsm",
   TabCrashHandler: "resource:///modules/ContentCrashHandlers.jsm",
   TelemetryEnvironment: "resource://gre/modules/TelemetryEnvironment.jsm",
+  TorConnect: "resource:///modules/TorConnect.jsm",
+  TorConnectState: "resource:///modules/TorConnect.jsm",
+  TorConnectTopics: "resource:///modules/TorConnect.jsm",
   Translation: "resource:///modules/translation/TranslationParent.jsm",
   UITour: "resource:///modules/UITour.jsm",
   UpdateUtils: "resource://gre/modules/UpdateUtils.jsm",
@@ -261,6 +264,16 @@ XPCOMUtils.defineLazyScriptGetter(
   this,
   "gSharedTabWarning",
   "chrome://browser/content/browser-webrtc.js"
+);
+XPCOMUtils.defineLazyScriptGetter(
+  this,
+  ["gTorConnectUrlbarButton"],
+  "chrome://browser/content/torconnect/torConnectUrlbarButton.js"
+);
+XPCOMUtils.defineLazyScriptGetter(
+  this,
+  ["gTorConnectTitlebarStatus"],
+  "chrome://browser/content/torconnect/torConnectTitlebarStatus.js"
 );
 XPCOMUtils.defineLazyScriptGetter(
   this,
@@ -650,6 +663,7 @@ var gPageIcons = {
 
 var gInitialPages = [
   "about:tor",
+  "about:torconnect",
   "about:blank",
   "about:home",
   ...(AppConstants.NIGHTLY_BUILD ? ["about:firefoxview"] : []),
@@ -1793,6 +1807,9 @@ var gBrowserInit = {
     // Init the NewIdentityButton
     NewIdentityButton.init();
 
+    gTorConnectUrlbarButton.init();
+    gTorConnectTitlebarStatus.init();
+
     gTorCircuitPanel.init();
 
     // Certain kinds of automigration rely on this notification to complete
@@ -2427,32 +2444,48 @@ var gBrowserInit = {
 
       let defaultArgs = BrowserHandler.defaultArgs;
 
-      // If the given URI is different from the homepage, we want to load it.
-      if (uri != defaultArgs) {
-        AboutNewTab.noteNonDefaultStartup();
+      // figure out which URI to actually load (or a Promise to get the uri)
+      uri = (aUri => {
+        // If the given URI is different from the homepage, we want to load it.
+        if (aUri != defaultArgs) {
+          AboutNewTab.noteNonDefaultStartup();
 
-        if (uri instanceof Ci.nsIArray) {
-          // Transform the nsIArray of nsISupportsString's into a JS Array of
-          // JS strings.
-          return Array.from(
-            uri.enumerate(Ci.nsISupportsString),
-            supportStr => supportStr.data
-          );
-        } else if (uri instanceof Ci.nsISupportsString) {
-          return uri.data;
+          if (aUri instanceof Ci.nsIArray) {
+            // Transform the nsIArray of nsISupportsString's into a JS Array of
+            // JS strings.
+            return Array.from(
+              aUri.enumerate(Ci.nsISupportsString),
+              supportStr => supportStr.data
+            );
+          } else if (aUri instanceof Ci.nsISupportsString) {
+            return aUri.data;
+          }
+          return aUri;
         }
-        return uri;
-      }
 
-      // The URI appears to be the the homepage. We want to load it only if
-      // session restore isn't about to override the homepage.
-      let willOverride = SessionStartup.willOverrideHomepage;
-      if (typeof willOverride == "boolean") {
-        return willOverride ? null : uri;
+        // The URI appears to be the the homepage. We want to load it only if
+        // session restore isn't about to override the homepage.
+        let willOverride = SessionStartup.willOverrideHomepage;
+        if (typeof willOverride == "boolean") {
+          return willOverride ? null : uri;
+        }
+        return willOverride.then(willOverrideHomepage =>
+          willOverrideHomepage ? null : uri
+        );
+      })(uri);
+
+      // if using TorConnect, convert these uris to redirects
+      if (TorConnect.shouldShowTorConnect) {
+        return Promise.resolve(uri).then(aUri => {
+          if (aUri == null) {
+            aUri = [];
+          }
+
+          aUri = TorConnect.getURIsToLoad(aUri);
+          return aUri;
+        });
       }
-      return willOverride.then(willOverrideHomepage =>
-        willOverrideHomepage ? null : uri
-      );
+      return uri;
     })());
   },
 
@@ -2518,6 +2551,9 @@ var gBrowserInit = {
     SecurityLevelButton.uninit();
 
     NewIdentityButton.uninit();
+
+    gTorConnectUrlbarButton.uninit();
+    gTorConnectTitlebarStatus.uninit();
 
     gTorCircuitPanel.uninit();
 
