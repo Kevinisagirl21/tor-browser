e3fe5ce02a55d412f52dc65bc5822ded78ef7c2b
Pier Angelo Vendrame <pierov@torproject.org>
2022-06-15 12:25:16 +0200

    fixup! Bug 11698: Incorporate Tor Browser Manual pages into Tor Browser
    Fixed flags of about:manual.

--- browser/components/about/AboutRedirector.cpp
+++ browser/components/about/AboutRedirector.cpp
@@ -142,7 +142,9 @@ static const RedirEntry kRedirMap[] = {
          nsIAboutModule::URI_CAN_LOAD_IN_CHILD | nsIAboutModule::ALLOW_SCRIPT |
          nsIAboutModule::HIDE_FROM_ABOUTABOUT},
     // The correct URI must be obtained by GetManualChromeURI
-    {"manual", "about:blank", nsIAboutModule::ALLOW_SCRIPT},
+    {"manual", "about:blank", nsIAboutModule::URI_SAFE_FOR_UNTRUSTED_CONTENT |
+         nsIAboutModule::ALLOW_SCRIPT | nsIAboutModule::URI_MUST_LOAD_IN_CHILD |
+         nsIAboutModule::URI_CAN_LOAD_IN_PRIVILEGEDABOUT_PROCESS},
 };
 
 static nsAutoCString GetAboutModuleName(nsIURI* aURI) {
