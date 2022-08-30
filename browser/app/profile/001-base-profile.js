294e26eec8fa1508832c2d4ed193faa906295dca
Pier Angelo Vendrame <pierov@torproject.org>
2022-08-30 11:28:28 +0200

    fixup! Firefox preference overrides.
    Enable HTTP/3 for Base Browser

--- browser/app/profile/001-base-profile.js
+++ browser/app/profile/001-base-profile.js
@@ -302,8 +302,6 @@ pref("network.connectivity-service.enabled", false);
 // As a "defense in depth" measure, configure an empty push server URL (the
 // DOM Push features are disabled by default via other prefs).
 pref("dom.push.serverURL", "");
-// Bug 40423: Disable http/3
-pref("network.http.http3.enabled", false);
 
 // Extension support
 pref("extensions.autoDisableScopes", 0);
