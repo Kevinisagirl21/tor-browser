e961ef8a040a6527afe1f23f5545cece94490939
Pier Angelo Vendrame <pierov@torproject.org>
2023-03-13 11:16:32 +0100

    Bug 41668: Tweaks to the Base Browser updater for Tor Browser
    This commit was once part of "Bug 4234: Use the Firefox Update Process
    for Tor Browser.".
    However, some parts of it were not needed for Base Browser and some
    derivative browsers.
    Therefore, we extracted from that commit the parts for Tor Browser
    legacy, and we add them back to the patch set with this commit.

--- toolkit/mozapps/update/UpdateService.sys.mjs
+++ toolkit/mozapps/update/UpdateService.sys.mjs
@@ -29,6 +29,12 @@ ChromeUtils.defineESModuleGetters(lazy, {
   setTimeout: "resource://gre/modules/Timer.sys.mjs",
 });
 
+ChromeUtils.defineModuleGetter(
+  lazy,
+  "TorMonitorService",
+  "resource://gre/modules/TorMonitorService.jsm"
+);
+
 XPCOMUtils.defineLazyServiceGetter(
   lazy,
   "AUS",
@@ -242,6 +248,7 @@ const SERVICE_ERRORS = [
 // Custom update error codes
 const BACKGROUNDCHECK_MULTIPLE_FAILURES = 110;
 const NETWORK_ERROR_OFFLINE = 111;
+const PROXY_SERVER_CONNECTION_REFUSED = 2152398920;
 
 // Error codes should be < 1000. Errors above 1000 represent http status codes
 const HTTP_ERROR_OFFSET = 1000;
@@ -382,6 +389,14 @@ XPCOMUtils.defineLazyGetter(
     return bts.isBackgroundTaskMode;
   }
 );
+
+function _shouldRegisterBootstrapObserver(errorCode) {
+  return (
+    errorCode == PROXY_SERVER_CONNECTION_REFUSED &&
+    !lazy.TorMonitorService.isBootstrapDone &&
+    lazy.TorMonitorService.ownsTorDaemon
+  );
+}
 
 /**
  * Changes `nsIApplicationUpdateService.currentState` and causes
@@ -2755,6 +2770,9 @@ UpdateService.prototype = {
       case "network:offline-status-changed":
         await this._offlineStatusChanged(data);
         break;
+      case "torconnect:bootstrap-complete":
+        this._bootstrapComplete();
+        break;
       case "nsPref:changed":
         if (data == PREF_APP_UPDATE_LOG || data == PREF_APP_UPDATE_LOG_FILE) {
           lazy.gLogEnabled; // Assigning this before it is lazy-loaded is an error.
@@ -3265,6 +3283,35 @@ UpdateService.prototype = {
     await this._attemptResume();
   },
 
+  _registerBootstrapObserver: function AUS__registerBootstrapObserver() {
+    if (this._registeredBootstrapObserver) {
+      LOG(
+        "UpdateService:_registerBootstrapObserver - observer already registered"
+      );
+      return;
+    }
+
+    LOG(
+      "UpdateService:_registerBootstrapObserver - waiting for tor bootstrap to " +
+        "be complete, then forcing another check"
+    );
+
+    Services.obs.addObserver(this, "torconnect:bootstrap-complete");
+    this._registeredBootstrapObserver = true;
+  },
+
+  _bootstrapComplete: function AUS__bootstrapComplete() {
+    Services.obs.removeObserver(this, "torconnect:bootstrap-complete");
+    this._registeredBootstrapObserver = false;
+
+    LOG(
+      "UpdateService:_bootstrapComplete - bootstrapping complete, forcing " +
+        "another background check"
+    );
+
+    this._attemptResume();
+  },
+
   /**
    * See nsIUpdateService.idl
    */
@@ -3297,6 +3344,11 @@ UpdateService.prototype = {
       if (this._pingSuffix) {
         AUSTLMY.pingCheckCode(this._pingSuffix, AUSTLMY.CHK_OFFLINE);
       }
+      return;
+    } else if (_shouldRegisterBootstrapObserver(update.errorCode)) {
+      // Register boostrap observer to try again, but only when we own the
+      // tor process.
+      this._registerBootstrapObserver();
       return;
     }
 
@@ -6441,6 +6493,7 @@ Downloader.prototype = {
     var state = this._patch.state;
     var shouldShowPrompt = false;
     var shouldRegisterOnlineObserver = false;
+    var shouldRegisterBootstrapObserver = false;
     var shouldRetrySoon = false;
     var deleteActiveUpdate = false;
     let migratedToReadyUpdate = false;
@@ -6571,7 +6624,20 @@ Downloader.prototype = {
       );
       shouldRegisterOnlineObserver = true;
       deleteActiveUpdate = false;
-
+    } else if (_shouldRegisterBootstrapObserver(status)) {
+      // Register a bootstrap observer to try again.
+      // The bootstrap observer will continue the incremental download by
+      // calling downloadUpdate on the active update which continues
+      // downloading the file from where it was.
+      LOG(
+        "Downloader:onStopRequest - not bootstrapped, register bootstrap observer: true"
+      );
+      AUSTLMY.pingDownloadCode(
+        this.isCompleteUpdate,
+        AUSTLMY.DWNLD_RETRY_OFFLINE
+      );
+      shouldRegisterBootstrapObserver = true;
+      deleteActiveUpdate = false;
       // Each of NS_ERROR_NET_TIMEOUT, ERROR_CONNECTION_REFUSED,
       // NS_ERROR_NET_RESET and NS_ERROR_DOCUMENT_NOT_CACHED can be returned
       // when disconnecting the internet while a download of a MAR is in
@@ -6694,7 +6760,11 @@ Downloader.prototype = {
 
     // Only notify listeners about the stopped state if we
     // aren't handling an internal retry.
-    if (!shouldRetrySoon && !shouldRegisterOnlineObserver) {
+    if (
+      !shouldRetrySoon &&
+      !shouldRegisterOnlineObserver &&
+      !shouldRegisterBootstrapObserver
+    ) {
       this.updateService.forEachDownloadListener(listener => {
         listener.onStopRequest(request, status);
       });
@@ -6893,6 +6963,9 @@ Downloader.prototype = {
     if (shouldRegisterOnlineObserver) {
       LOG("Downloader:onStopRequest - Registering online observer");
       this.updateService._registerOnlineObserver();
+    } else if (shouldRegisterBootstrapObserver) {
+      LOG("Downloader:onStopRequest - Registering bootstrap observer");
+      this.updateService._registerBootstrapObserver();
     } else if (shouldRetrySoon) {
       LOG("Downloader:onStopRequest - Retrying soon");
       this.updateService._consecutiveSocketErrors++;
