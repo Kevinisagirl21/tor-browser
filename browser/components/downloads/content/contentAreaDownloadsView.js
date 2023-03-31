a63c90b7dacb29faf91eb273904614ddec7caf9b
Dan Ballard <dan@mindstab.net>
2023-03-31 12:35:17 -0500

    Bug 40701: Add security warning when downloading a file
    Shown in the downloads panel, about:downloads and places.xhtml.

--- browser/components/downloads/content/contentAreaDownloadsView.js
+++ browser/components/downloads/content/contentAreaDownloadsView.js
@@ -10,6 +10,9 @@ const { PrivateBrowsingUtils } = ChromeUtils.importESModule(
 
 var ContentAreaDownloadsView = {
   init() {
+    const torWarningMessage = document.getElementById(
+      "aboutDownloadsTorWarning"
+    );
     let box = document.getElementById("downloadsListBox");
     let suppressionFlag = DownloadsCommon.SUPPRESS_CONTENT_AREA_DOWNLOADS_OPEN;
     box.addEventListener(
@@ -17,9 +20,22 @@ var ContentAreaDownloadsView = {
       () => {
         // Set focus to Downloads list once it is created
         // And prevent it from showing the focus ring around the richlistbox (Bug 1702694)
-        document
-          .getElementById("downloadsListBox")
-          .focus({ focusVisible: false });
+        // Prevent focusing the list whilst the tor browser warning is shown.
+        // Some screen readers (tested with Orca and NVDA) will not read out
+        // alerts if they are already present on page load. In that case, a
+        // screen reader user may not be aware of the warning before they
+        // interact with the downloads list, which we do not want.
+        // Some hacky workarounds were tested with Orca to get it to read back
+        // the alert before the focus is read, but this was inconsistent and the
+        // experience was bad.
+        // Without auto-focusing the downloads list, a screen reader should not
+        // skip beyond the alert's content.
+        if (torWarningMessage.hidden) {
+          document
+            .getElementById("downloadsListBox")
+            .focus({ focusVisible: false });
+        }
+
         // Pause the indicator if the browser is active.
         if (document.visibilityState === "visible") {
           DownloadsCommon.getIndicatorData(window).attentionSuppressed |=
@@ -28,7 +44,12 @@ var ContentAreaDownloadsView = {
       },
       { once: true }
     );
-    let view = new DownloadsPlacesView(box, true, suppressionFlag);
+    let view = new DownloadsPlacesView(
+      box,
+      torWarningMessage,
+      true,
+      suppressionFlag
+    );
     document.addEventListener("visibilitychange", aEvent => {
       let indicator = DownloadsCommon.getIndicatorData(window);
       if (document.visibilityState === "visible") {
@@ -41,6 +62,53 @@ var ContentAreaDownloadsView = {
     if (!PrivateBrowsingUtils.isContentWindowPrivate(window)) {
       view.place = "place:transition=7&sort=4";
     }
+
+    torWarningMessage.querySelector(
+      ".downloads-tor-warning-title"
+    ).textContent = this._getTorString("torbutton.download.warning.title");
+
+    const tailsLink = document.createElement("a");
+    tailsLink.href = "https://tails.net/";
+    tailsLink.target = "_blank";
+    tailsLink.textContent = this._getTorString(
+      "torbutton.download.warning.tails_brand_name"
+    );
+
+    const [beforeLink, afterLink] = this._getTorString(
+      "torbutton.download.warning.description"
+    ).split("%S");
+
+    torWarningMessage
+      .querySelector(".downloads-tor-warning-description")
+      .append(beforeLink, tailsLink, afterLink);
+
+    torWarningMessage.querySelector(
+      ".downloads-tor-warning-dismiss-button"
+    ).textContent = this._getTorString("torbutton.download.warning.dismiss");
+  },
+
+  /**
+   * Get a string from the properties bundle.
+   *
+   * @param {string} name - The string name.
+   *
+   * @return {string} The string.
+   */
+  _getTorString(name) {
+    if (!this._stringBundle) {
+      this._stringBundle = Services.strings.createBundle(
+        "chrome://torbutton/locale/torbutton.properties"
+      );
+    }
+    try {
+      return this._stringBundle.GetStringFromName(name);
+    } catch {}
+    if (!this._fallbackStringBundle) {
+      this._fallbackStringBundle = Services.strings.createBundle(
+        "resource://torbutton/locale/en-US/torbutton.properties"
+      );
+    }
+    return this._fallbackStringBundle.GetStringFromName(name);
   },
 };
 
