1aeaceb827550211ed7017a2b3e982485377c6ff
Pier Angelo Vendrame <pierov@torproject.org>
2023-06-14 16:35:13 +0200

    fixup! Bug 7494: Create local home page for TBB.
    First step toward IPC modernizartion (WIP).

--- browser/components/abouttor/AboutTorChild.sys.mjs
+++ browser/components/abouttor/AboutTorChild.sys.mjs
@@ -1,2 +1,26 @@
 export class AboutTorChild extends JSWindowActorChild {
+  handleEvent(e) {
+    switch (e.type) {
+      case "DOMContentLoaded":
+        this.sendAsyncMessage("AboutTor:ContentLoaded");
+    }
+  }
+
+  receiveMessage(message) {
+    switch (message.name) {
+      case "AboutTor:ChromeData":
+        this.#sendToContent("ChromeData", message.data);
+        break;
+      case "AboutTor:LocaleData":
+        this.#sendToContent("LocaleData", message.data);
+        break;
+    }
+  }
+
+  #sendToContent(eventName, detail) {
+    let event = new this.contentWindow.CustomEvent(eventName, {
+      detail: Cu.cloneInto(detail, this.contentWindow),
+    });
+    this.contentWindow.dispatchEvent(event);
+  }
 }
--- browser/components/abouttor/AboutTorParent.sys.mjs
+++ browser/components/abouttor/AboutTorParent.sys.mjs
@@ -1,2 +1,31 @@
 export class AboutTorParent extends JSWindowActorParent {
+  async receiveMessage(message) {
+    if (message.name === "AboutTor:ContentLoaded") {
+      this.#sendChromeData();
+      this.#sendLocale();
+    }
+  }
+
+  #sendChromeData() {
+    const tbbVersion = Services.prefs.getCharPref("torbrowser.version");
+    this.sendAsyncMessage("AboutTor:ChromeData", {
+      tbbVersion,
+      torOn: true,
+      updateChannel: "alpha",
+      hasBeenUpdated: true,
+    });
+  }
+
+  #sendLocale() {
+    try {
+      const kBrandBundle = "chrome://branding/locale/brand.properties";
+      const brandBundle = Services.strings.createBundle(kBrandBundle);
+      const productName = brandBundle.GetStringFromName("brandFullName");
+      let locale = Services.locale.appLocaleAsBCP47;
+      if (locale === "ja-JP-macos") {
+        locale = "ja";
+      }
+      this.sendAsyncMessage("AboutTor:LocaleData", { locale, productName });
+    } catch (e) {}
+  }
 }
