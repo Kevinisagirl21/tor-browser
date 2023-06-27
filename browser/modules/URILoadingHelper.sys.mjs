e286ad1ef5461e3d60f05a6fcb32fce8d2011656
Pier Angelo Vendrame <pierov@torproject.org>
2023-06-27 15:51:49 +0200

    fixup! Bug 27476: Implement about:torconnect captive portal within Tor Browser
    Moved the logic of the about:tor redirects before the bootstrap

--- browser/modules/URILoadingHelper.sys.mjs
+++ browser/modules/URILoadingHelper.sys.mjs
@@ -418,6 +418,28 @@ export const URILoadingHelper = {
       return;
     }
 
+    // make sure users are not faced with the scary red 'tor isn't working' screen
+    // if they navigate to about:tor before bootstrapped
+    //
+    // fixes tor-browser#40752
+    // new tabs also redirect to about:tor if browser.newtabpage.enabled is true
+    // otherwise they go to about:blank
+    if (lazy.TorConnect.shouldShowTorConnect) {
+      const homeURLs = [
+        "about:home",
+        "about:privatebrowsing",
+        "about:tor",
+        "about:welcome",
+      ];
+      if (
+        homeURLs.includes(url) ||
+        (url === "about:newtab" &&
+          Services.prefs.getBoolPref("browser.newtabpage.enabled", false))
+      ) {
+        url = lazy.TorConnect.getRedirectURL(url);
+      }
+    }
+
     let {
       allowThirdPartyFixup,
       postData,
@@ -672,22 +694,6 @@ export const URILoadingHelper = {
     aReferrerInfo
   ) {
     event = BrowserUtils.getRootEvent(event);
-
-    // make sure users are not faced with the scary red 'tor isn't working' screen
-    // if they navigate to about:tor before bootstrapped
-    //
-    // fixes tor-browser#40752
-    // new tabs also redirect to about:tor if browser.newtabpage.enabled is true
-    // otherwise they go to about:blank
-    if (lazy.TorConnect.shouldShowTorConnect) {
-      if (
-        url === "about:tor" ||
-        (url === "about:newtab" &&
-          Services.prefs.getBoolPref("browser.newtabpage.enabled", false))
-      ) {
-        url = lazy.TorConnect.getRedirectURL(url);
-      }
-    }
 
     let params;
 
