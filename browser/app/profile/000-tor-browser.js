da33ad3bc03cc015d404b6907978a82585518259
Pier Angelo Vendrame <pierov@torproject.org>
2023-07-20 19:22:00 +0200

    fixup! Bug 40562: Added Tor Browser preferences to 000-tor-browser.js
    Bug 41872: Use IPC by default on Linux and macOS.
    
    The preference is flipped also for Windows, but it is actually ignored.

--- browser/app/profile/000-tor-browser.js
+++ browser/app/profile/000-tor-browser.js
@@ -74,28 +74,23 @@ pref("extensions.torlauncher.quickstart", false);
 
 pref("extensions.torlauncher.max_tor_log_entries", 1000);
 
-// By default, Tor Launcher configures a TCP listener for the Tor
-// control port, as defined by control_host and control_port.
-// Set control_port_use_ipc to true to use an IPC object (e.g., a Unix
-// domain socket) instead. You may also modify control_ipc_path to
-// override the default IPC object location. If a relative path is used,
-// it is handled like torrc_path (see below).
+// On Linux and macOS, Tor Browser can use IPC for the communication with the
+// tor daemon, and this is the preferred way.
+// You can set the path of the Unix sockets with control_ipc_path.
+// If a relative one is provided, it is handled like torrc_path (see below).
+// Tor Browser on Windows will use TCP (and it can be used also on Linux and
+// macOS, if preferred).
+// Host and port can be customized with control_host and control_port.
+// This applies both to the control port and to the SOCKS port, but their
+// configurations are completely independent.
+pref("extensions.torlauncher.control_port_use_ipc", true);
+pref("extensions.torlauncher.control_ipc_path", "");
 pref("extensions.torlauncher.control_host", "127.0.0.1");
 pref("extensions.torlauncher.control_port", 9151);
-pref("extensions.torlauncher.control_port_use_ipc", false);
-pref("extensions.torlauncher.control_ipc_path", "");
-
-// By default, Tor Launcher configures a TCP listener for the Tor
-// SOCKS port. The host is taken from the network.proxy.socks pref and
-// the port is taken from the network.proxy.socks_port pref.
-// Set socks_port_use_ipc to true to use an IPC object (e.g., a Unix
-// domain socket) instead. You may also modify socks_ipc_path to
-// override the default IPC object location. If a relative path is used,
-// it is handled like torrc_path (see below).
+pref("extensions.torlauncher.socks_port_use_ipc", true);
+pref("extensions.torlauncher.socks_ipc_path", "");
 // Modify socks_port_flags to use a different set of SocksPort flags (but be
 // careful).
-pref("extensions.torlauncher.socks_port_use_ipc", false);
-pref("extensions.torlauncher.socks_ipc_path", "");
 pref("extensions.torlauncher.socks_port_flags", "ExtendedErrors IPv6Traffic PreferIPv6 KeepAliveIsolateSOCKSAuth");
 
 // The tor_path is relative to the application directory. On Linux and
