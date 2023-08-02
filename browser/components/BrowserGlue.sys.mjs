a1051f4e64583983707d34ff1c3c49434ca52d7e
Henry Wilkes <henry@torproject.org>
2023-08-02 12:18:08 +0100

    Bug 7494: Create local home page for TBB.
    Bug 41333: Update about:tor to new design. Including:
    + make the favicon match the branding icon.
    + make the location bar show a search icon.

--- browser/components/BrowserGlue.sys.mjs
+++ browser/components/BrowserGlue.sys.mjs
@@ -102,6 +102,7 @@ ChromeUtils.defineESModuleGetters(lazy, {
   WindowsLaunchOnLogin: "resource://gre/modules/WindowsLaunchOnLogin.sys.mjs",
   WindowsRegistry: "resource://gre/modules/WindowsRegistry.sys.mjs",
   WindowsGPOParser: "resource://gre/modules/policies/WindowsGPOParser.sys.mjs",
+  checkHomepageOverride: "resource:///modules/HomepageOverride.sys.mjs",
   clearTimeout: "resource://gre/modules/Timer.sys.mjs",
   setTimeout: "resource://gre/modules/Timer.sys.mjs",
 });
@@ -499,6 +500,22 @@ let JSWINDOWACTORS = {
     },
 
     matches: ["about:tabcrashed*"],
+  },
+
+  AboutTor: {
+    parent: {
+      esModuleURI: "resource:///actors/AboutTorParent.sys.mjs",
+    },
+    child: {
+      esModuleURI: "resource:///actors/AboutTorChild.sys.mjs",
+
+      events: {
+        DOMContentLoaded: {},
+        SubmitSearchOnionize: { wantUntrusted: true },
+      },
+    },
+
+    matches: ["about:tor"],
   },
 
   AboutWelcomeShopping: {
@@ -1616,6 +1633,8 @@ BrowserGlue.prototype = {
     }
 
     lazy.ResetPBMPanel.init();
+
+    lazy.checkHomepageOverride();
 
     AboutHomeStartupCache.init();
 
