5ff3177a60a98b44b99e488795c59759460ee38e
Pier Angelo Vendrame <pierov@torproject.org>
2023-08-18 16:31:38 +0200

    Bug 42027: Base Browser migration procedures.
    This commit implmenents the the Base Browser's version of _migrateUI.

--- browser/components/BrowserGlue.sys.mjs
+++ browser/components/BrowserGlue.sys.mjs
@@ -1300,6 +1300,9 @@ BrowserGlue.prototype = {
     // handle any UI migration
     this._migrateUI();
 
+    // Base Browser-specific version of _migrateUI.
+    this._migrateUIBB();
+
     // Handle any TBB-specific migration before showing the UI. Keep after
     // _migrateUI to make sure this._isNewProfile has been defined.
     this._migrateUITBB();
@@ -4402,6 +4405,25 @@ BrowserGlue.prototype = {
 
     // Update the migration version.
     Services.prefs.setIntPref("browser.migration.version", UI_VERSION);
+  },
+
+  _migrateUIBB() {
+    // Version 1: 13.0a3. Reset layout.css.prefers-color-scheme.content-override
+    //            for tor-browser#41739.
+    const MIGRATION_VERSION = 1;
+    const MIGRATION_PREF = "basebrowser.migration.version";
+    // We do not care whether this is a new or old profile, since in version 1
+    // we just quickly clear a user preference, which should not do anything to
+    // new profiles.
+    // Shall we ever raise the version number and have a watershed, we can add
+    // a check easily (any version > 0 will be an old profile).
+    const currentVersion = Services.prefs.getIntPref(MIGRATION_PREF, 0);
+    if (currentVersion < 1) {
+      Services.prefs.clearUserPref(
+        "layout.css.prefers-color-scheme.content-override"
+      );
+    }
+    Services.prefs.setIntPref(MIGRATION_PREF, MIGRATION_VERSION);
   },
 
   // Use this method for any TBB migration that can be run just before showing
