Floorp-Projects/Floorp

Bug 1836353 - make mStartedReading an Atomic r=valentin,necko-reviewers a=pascalc
Differential Revision: https://phabricator.services.mozilla.com/D187484

netwerk/base/nsMIMEInputStream.cpp
@@ -57,8 +57,6 @@ class nsMIMEInputStream : public nsIMIMEInputStream,
   NS_DECL_NSICLONEABLEINPUTSTREAM
 
  private:
-  void InitStreams();
-
   struct MOZ_STACK_CLASS ReadSegmentsState {
     nsCOMPtr<nsIInputStream> mThisStream;
     nsWriteSegmentFun mWriter{nullptr};
@@ -77,12 +75,10 @@ class nsMIMEInputStream : public nsIMIMEInputStream,
   nsTArray<HeaderEntry> mHeaders;
 
   nsCOMPtr<nsIInputStream> mStream;
-  bool mStartedReading{false};
+  mozilla::Atomic<bool, mozilla::Relaxed> mStartedReading{false};
 
-  mozilla::Mutex mMutex MOZ_UNANNOTATED{"nsMIMEInputStream::mMutex"};
-
-  // This is protected by mutex.
-  nsCOMPtr<nsIInputStreamCallback> mAsyncWaitCallback;
+  mozilla::Mutex mMutex{"nsMIMEInputStream::mMutex"};
+  nsCOMPtr<nsIInputStreamCallback> mAsyncWaitCallback MOZ_GUARDED_BY(mMutex);
 
   // This is protected by mutex.
   nsCOMPtr<nsIInputStreamLengthCallback> mAsyncInputStreamLengthCallback;
@@ -158,18 +154,10 @@ nsMIMEInputStream::GetData(nsIInputStream** aStream) {
   return NS_OK;
 }
 
-// set up the internal streams
-void nsMIMEInputStream::InitStreams() {
-  NS_ASSERTION(!mStartedReading,
-               "Don't call initStreams twice without rewinding");
-
-  mStartedReading = true;
-}
-
 #define INITSTREAMS                               \
   if (!mStartedReading) {                         \
     NS_ENSURE_TRUE(mStream, NS_ERROR_UNEXPECTED); \
-    InitStreams();                                \
+    mStartedReading = true;                       \
   }
 
 // Reset mStartedReading when Seek-ing to start
@@ -215,7 +203,7 @@ nsresult nsMIMEInputStream::ReadSegCb(nsIInputStream* aIn, void* aClosure,
 }
 
 /**
- * Forward everything else to the mStream after calling InitStreams()
+ * Forward everything else to the mStream after calling INITSTREAMS
  */
 
 // nsIInputStream
@@ -505,7 +493,7 @@ nsMIMEInputStream::Clone(nsIInputStream** aResult) {
   }
 
   static_cast<nsMIMEInputStream*>(mimeStream.get())->mStartedReading =
-      mStartedReading;
+      static_cast<bool>(mStartedReading);
 
   mimeStream.forget(aResult);
   return NS_OK;

Kevinisagirl21/tor-browser

ESR102 backport amend: removed MOZ_ prefix from Bug 1761040

netwerk/base/nsMIMEInputStream.cpp
@@ -57,8 +57,6 @@ class nsMIMEInputStream : public nsIMIMEInputStream,
   NS_DECL_NSICLONEABLEINPUTSTREAM
 
  private:
-  void InitStreams();
-
   struct MOZ_STACK_CLASS ReadSegmentsState {
     nsCOMPtr<nsIInputStream> mThisStream;
     nsWriteSegmentFun mWriter{nullptr};
@@ -77,12 +75,10 @@ class nsMIMEInputStream : public nsIMIMEInputStream,
   nsTArray<HeaderEntry> mHeaders;
 
   nsCOMPtr<nsIInputStream> mStream;
-  bool mStartedReading{false};
+  mozilla::Atomic<bool, mozilla::Relaxed> mStartedReading{false};
 
-  mozilla::Mutex mMutex MOZ_UNANNOTATED{"nsMIMEInputStream::mMutex"};
-
-  // This is protected by mutex.
-  nsCOMPtr<nsIInputStreamCallback> mAsyncWaitCallback;
+  mozilla::Mutex mMutex{"nsMIMEInputStream::mMutex"};
+  nsCOMPtr<nsIInputStreamCallback> mAsyncWaitCallback GUARDED_BY(mMutex);
 
   // This is protected by mutex.
   nsCOMPtr<nsIInputStreamLengthCallback> mAsyncInputStreamLengthCallback;
@@ -158,18 +154,10 @@ nsMIMEInputStream::GetData(nsIInputStream** aStream) {
   return NS_OK;
 }
 
-// set up the internal streams
-void nsMIMEInputStream::InitStreams() {
-  NS_ASSERTION(!mStartedReading,
-               "Don't call initStreams twice without rewinding");
-
-  mStartedReading = true;
-}
-
 #define INITSTREAMS                               \
   if (!mStartedReading) {                         \
     NS_ENSURE_TRUE(mStream, NS_ERROR_UNEXPECTED); \
-    InitStreams();                                \
+    mStartedReading = true;                       \
   }
 
 // Reset mStartedReading when Seek-ing to start
@@ -215,7 +203,7 @@ nsresult nsMIMEInputStream::ReadSegCb(nsIInputStream* aIn, void* aClosure,
 }
 
 /**
- * Forward everything else to the mStream after calling InitStreams()
+ * Forward everything else to the mStream after calling INITSTREAMS
  */
 
 // nsIInputStream
@@ -501,7 +489,7 @@ nsMIMEInputStream::Clone(nsIInputStream** aResult) {
   }
 
   static_cast<nsMIMEInputStream*>(mimeStream.get())->mStartedReading =
-      mStartedReading;
+      static_cast<bool>(mStartedReading);
 
   mimeStream.forget(aResult);
   return NS_OK;