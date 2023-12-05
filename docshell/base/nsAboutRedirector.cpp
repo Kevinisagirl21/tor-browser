5864505174fc8886b5b4fad54f51f2c7d7d8091e
Pier Angelo Vendrame <pierov@torproject.org>
2023-12-05 13:39:43 +0100

    fixup! Bug 27476: Implement about:torconnect captive portal within Tor Browser
    HTML-ize about:torconnect.
    
    Changed file extension, updated the HTML tag, removed short-tags on tags
    that are expected to have a closing tag.
    Also, removed the preprocessor and moved the onion pattern to this
    commit, since it is used only here.

--- docshell/base/nsAboutRedirector.cpp
+++ docshell/base/nsAboutRedirector.cpp
@@ -169,7 +169,7 @@ static const RedirEntry kRedirMap[] = {
 #endif
     {"telemetry", "chrome://global/content/aboutTelemetry.xhtml",
      nsIAboutModule::ALLOW_SCRIPT | nsIAboutModule::IS_SECURE_CHROME_UI},
-    {"torconnect", "chrome://global/content/torconnect/aboutTorConnect.xhtml",
+    {"torconnect", "chrome://global/content/torconnect/aboutTorConnect.html",
      nsIAboutModule::URI_SAFE_FOR_UNTRUSTED_CONTENT |
          nsIAboutModule::URI_CAN_LOAD_IN_CHILD | nsIAboutModule::ALLOW_SCRIPT |
          nsIAboutModule::HIDE_FROM_ABOUTABOUT |
