825a3807fea100fbcbfb750d5a8d4156829d529c
Henry Wilkes <henry@torproject.org>
2024-02-06 10:46:08 +0000

    fixup! Bug 31286: Implementation of bridge, proxy, and firewall settings in about:preferences#connection
    Bug 42036: New QR code design.

--- browser/components/torpreferences/content/bridgeQrDialog.js
+++ browser/components/torpreferences/content/bridgeQrDialog.js
@@ -19,15 +19,18 @@ window.addEventListener(
     );
     const target = document.getElementById("bridgeQr-target");
     const style = window.getComputedStyle(target);
-    const width = style.width.substr(0, style.width.length - 2);
-    const height = style.height.substr(0, style.height.length - 2);
+    // We are assuming that the style width and height have "px" units.
+    // Trailing "px" is not parsed.
+    // NOTE: Our QRCode module doesn't seem to use the width or height
+    // attributes.
+    const width = parseInt(style.width, 10);
+    const height = parseInt(style.height, 10);
     new QRCode(target, {
       text: bridgeString,
       width,
       height,
       colorDark: style.color,
       colorLight: style.backgroundColor,
-      document,
     });
   },
   { once: true }
