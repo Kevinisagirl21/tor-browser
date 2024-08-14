946776bbb93a576a015e5a42afac3e447e4d6df1
Dan Ballard <dan@mindstab.net>
2024-08-13 18:24:17 -0700

    fixup! Bug 41878: [android] Add standalone Tor Bootstrap
    comment

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/SettingsFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/SettingsFragment.kt
@@ -197,6 +197,7 @@ class SettingsFragment : PreferenceFragmentCompat() {
         requireView().findViewById<RecyclerView>(R.id.recycler_view)
             .also {
                 it?.hideInitialScrollBar(viewLifecycleOwner.lifecycleScope)
+                // Prevent disabled settings from having a collapsing animation on open
                 it?.disableHidingAnimation()
             }
 
