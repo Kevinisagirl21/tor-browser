604b214075342c8d03f44184d5dd238d3d94060e
Emilio Cobos Álvarez <emilio@crisal.io>
2024-08-22 05:39:07 +0000

    Bug 1914106 - Deal with insertRule edge-case. r=jwatt a=RyanVM
    When there's trailing garbage after an @import rule we throw, but we
    still trigger the load (that's not great but not trivial to change).
    
    Deal with that case before calling ImportRuleLoaded().
    
    Differential Revision: https://phabricator.services.mozilla.com/D219783

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -8326,7 +8326,7 @@ void Document::RuleAdded(StyleSheet& aSheet, css::Rule& aRule) {
   }
 }
 
-void Document::ImportRuleLoaded(dom::CSSImportRule& aRule, StyleSheet& aSheet) {
+void Document::ImportRuleLoaded(StyleSheet& aSheet) {
   if (aSheet.IsApplicable()) {
     ApplicableStylesChanged();
   }
