e8eefc2544e50ec8a812e738c6a9540b5292f0d0
Henry Wilkes <henry@torproject.org>
2024-08-27 16:51:30 +0100

    fixup! Bug 7494: Create local home page for TBB.
    Bug 42744: Stop using prefersColorSchemeOverride for about:tor.
    
    common-shared.css seems to have simplified since the original
    implementation, so we can simply set the `color-scheme` and `color` on
    the search form to keep the styling consistent in both light and dark
    themes.

--- browser/components/abouttor/AboutTorChild.sys.mjs
+++ browser/components/abouttor/AboutTorChild.sys.mjs
@@ -2,19 +2,6 @@
  * Actor child class for the about:tor page.
  */
 export class AboutTorChild extends JSWindowActorChild {
-  actorCreated() {
-    if (this.contentWindow.matchMedia("not (prefers-contrast)").matches) {
-      // When prefers-contrast is not set, the page only has one style because
-      // we always set a dark background and a light <form>.
-      // We force prefers-color-scheme to be light, regardless of the user's
-      // settings so that we inherit the "light" theme styling from
-      // in-content/common.css for the <form> element. In particular, we want
-      // the light styling for the <input> and <moz-toggle> elements, which are
-      // on a light background.
-      this.browsingContext.prefersColorSchemeOverride = "light";
-    }
-  }
-
   handleEvent(event) {
     switch (event.type) {
       case "DOMContentLoaded":
