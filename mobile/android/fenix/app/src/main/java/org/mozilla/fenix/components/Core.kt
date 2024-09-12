6ec046c6164a62b07f8c5b1a14d5acba814be297
Dan Ballard <dan@mindstab.net>
2024-09-12 10:55:16 -0700

    fixup! [android] Disable features and functionality
    Bug 43046: Disable emailTrackingBlocking in pbm

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/Core.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/Core.kt
@@ -156,7 +156,7 @@ class Core(
             cookieBannerHandlingDetectOnlyMode = context.settings().shouldEnableCookieBannerDetectOnly,
             cookieBannerHandlingGlobalRules = context.settings().shouldEnableCookieBannerGlobalRules,
             cookieBannerHandlingGlobalRulesSubFrames = context.settings().shouldEnableCookieBannerGlobalRulesSubFrame,
-            emailTrackerBlockingPrivateBrowsing = true,
+            emailTrackerBlockingPrivateBrowsing = false,
             torSecurityLevel = context.settings().torSecurityLevel().intRepresentation,
             spoofEnglish = context.settings().spoofEnglish,
         )
