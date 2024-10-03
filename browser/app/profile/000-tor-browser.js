39263a4000c5756cb63844a1bebe2b827a892511
Morgan <morgan@torproject.org>
2024-10-03 18:57:03 +0000

    fixup! Bug 40562: Added Tor Browser preferences to 000-tor-browser.js
    Bug 43170: Disable user-agent spoofing in HTTP header

--- browser/app/profile/000-tor-browser.js
+++ browser/app/profile/000-tor-browser.js
@@ -52,6 +52,8 @@ pref("network.http.connection-retry-timeout", 0);
 // be reduced to the strictly required time).
 pref("extensions.torbutton.use_nontor_proxy", false);
 
+// tor-browser#42647: Make OS HTTP User-Agent OS spoofing configurable by pref
+pref("privacy.resistFingerprinting.spoofOsInUserAgentHeader", true);
 // Browser home page:
 pref("browser.startup.homepage", "about:tor");
 
